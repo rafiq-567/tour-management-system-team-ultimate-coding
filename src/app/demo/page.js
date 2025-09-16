ee7336a3b751847bafca401294587635155915b4
hrhabib0 <hrhabib398668@gmail.com>
2025-09-16 17:08:06 +0600

    Add DemoComponent for Next.js Project  GitHub branch test

---
+++ src/app/demo/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+export default function DemoComponent() {
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-800">
+      <h1 className="text-4xl font-bold mb-4">🚀 Tour Management System</h1>
+      <p className="text-lg text-gray-600 mb-2">
+        This is a demo component for testing GitHub branching & commits.
+      </p>
+    </div>
+  );
+}
