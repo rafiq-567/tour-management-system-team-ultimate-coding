ae654470aed8abdead9f4febb47893172ea9b7ec
Khairul <khairul.web1@gmail.com>
2025-09-16 09:24:50 +0600

    login page ok

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NextAuthSessionProvider from "@/Providers/authProvider/NextAuthSessionProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,11 +20,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <NextAuthSessionProvider>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
+      </NextAuthSessionProvider>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import LoginButton from "./components/loginButton/LoginButton";
 
 export default function Home() {
   return (
-   <div>hello ultimate coding team members</div>
+   <div>
+    <h3>hello ultimate coding team members</h3>
+    <LoginButton></LoginButton>
+   </div>
   );
 }
