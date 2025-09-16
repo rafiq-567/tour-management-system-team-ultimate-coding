4c89d904622863688ae6b76ebc033f1398ab57aa
Humaon Kabir <humaonkabir2003@gmail.com>
2025-09-16 18:49:39 +0600

    hero section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import DestinationsList from "@/components/Home/DestinationCard";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        
         {children}
+       < DestinationsList></DestinationsList>
       </body>
     </html>
   );
