58392a59bd673c5ab4affc9f95c6b9e0424659cb
MD. RAFIQUL AKHTER <mdrafiqulakhter0@gmail.com>
2025-09-17 01:27:47 +0600

    Revert "Kabir"

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,7 @@
-import Test from "@/components/Test";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>hello ultimate coding team members
-    <div>
-      <Test></Test>
-    </div>
-   </div>
+   <div>hello ultimate coding team members</div>
   );
 }
