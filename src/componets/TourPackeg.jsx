cce7d1d5a8aed4cdf7a104d55cbdf1a56dffa9fc
abul kalam <bd.abulkalam800@gamil.com>
2025-09-16 09:46:01 +0300

    create tour packeg page

---
+++ src/componets/TourPackeg.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TourPackeg = () => {
+    return (
+        <div>
+            <h1>Create your Best tour packege</h1>
+        </div>
+    );
+};
+
+export default TourPackeg;
