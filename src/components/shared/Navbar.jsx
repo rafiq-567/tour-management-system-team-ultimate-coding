7eca0f5626c22c3b7773d66b13c6b7658c42b6a2
Humaon Kabir <humaonkabir2003@gmail.com>
2025-09-22 12:09:11 +0600

    Fresh Check  kabir branch navbar check

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Hello, Navbar!</h1>
+            
+        </div>
+    );
+};
+
+export default Navbar;
