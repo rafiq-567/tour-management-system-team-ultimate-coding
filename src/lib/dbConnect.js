59440db9ff3c7e3c6164d3d014f947ee946a4921
abul kalam <bd.abulkalam800@gamil.com>
2025-10-07 08:58:46 +0300

    my old code

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,20 +1,19 @@
-'use server'
+"use server";
 import { MongoClient, ServerApiVersion } from "mongodb";
 // database connection start here abul kalam ***********************
 // import React from 'react';
 
-function dbConnect(collectionName){
-
-    const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
-    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-    const client = new MongoClient(uri, {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-            maxPoolSize: 10,
-        },
-    });
-    return client.db(process.env.DB_NAME).collection(collectionName)
+function dbConnect(collectionName) {
+  const uri = process.env.MONGODB_URL;
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+      maxPoolSize: 10,
+    },
+  });
+  return client.db(process.env.DB_NAME).collection(collectionName);
 }
 export default dbConnect;
